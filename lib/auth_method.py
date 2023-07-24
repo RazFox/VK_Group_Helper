83c7ac4005b7750003caeca3ecb8f9caebcfb6c7
Ruslan_B <ruslan.bakiev.nc@gmail.com>
2023-07-24 19:42:33 +0300

    class VKAuthentication init Data for storing authentication in VK

---
+++ lib/auth_method.py
@@ -0,0 +1,21 @@
+import os.path
+from dotenv import load_dotenv
+
+
+class VKAuthentication:
+
+    def __init__(self):
+        self.token = None
+        self.v_api = None
+        self.group_id = None
+        self.owner_id = None
+
+    def settings_read(self, path_env):
+        if os.path.isfile(path_env):
+            load_dotenv(path_env)
+            self.token = os.getenv("VK_TOKEN")
+            self.v_api = os.getenv("VK_API")
+            self.group_id = os.getenv("VK_GROUP_ID")
+            self.owner_id = os.getenv("VK_OWNER_ID")
+        else:
+            raise OSError
